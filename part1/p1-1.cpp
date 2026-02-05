440256b17896bd8ddbf842cc98379280385765df
judy326879 <judy.chang326@gmail.com>
2026-02-06 03:10:12 +0800

    first try

---
+++ part1/p1-1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <stack>
+#include <list>
+#include <string>
+using namespace std;
+stack<int> st;
+list<string> li;
+int main(){
+    string n;
+    while(cin >> n) li.push_back(n);
+    int len = li.size();
+    for(int i=0;i<len;++i){
+        string s = li.back();
+        li.pop_back();
+        if(s == "f"&&!st.empty()){
+            int n1 = st.top();
+            st.pop();
+            st.push(2*n1-1);
+        }
+        else if(s == "g"&&!st.empty()){
+            int n1 = st.top();
+            st.pop();
+            int n2 = st.top();
+            st.pop();
+            st.push(n1+2*n2-3);
+        }
+        else{
+            st.push(stoi(s));
+        }
+    }
+    int k = st.top();
+    cout << k << endl;
+}
+
+proudly powered by DMOJ | Cop
